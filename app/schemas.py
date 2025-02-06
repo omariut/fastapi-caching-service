82bf8ef8d3f9be6179adaac26107570750eda329
omariut <omarcpgcbl@gmail.com>
2025-02-06 18:35:37 +0600

    feat : add equal length match validator for lists in payload create request

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,16 @@
+from pydantic import BaseModel, model_validator
 from typing import List
-import uuid
-from pydantic import BaseModel
+from typing_extensions import Self
 
 class PayloadCreate(BaseModel):
     list_1: List[str]
     list_2: List[str]
+
+    @model_validator(mode='after')
+    def check_length_match(self) -> Self:
+        if len(self.list_1) != len(self.list_2):
+            raise ValueError("Lists must be of the same length")
+        return self
 
 class PayloadRead(BaseModel):
     id: int
